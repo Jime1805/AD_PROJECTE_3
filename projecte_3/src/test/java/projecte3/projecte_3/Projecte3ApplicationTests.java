9f3b8168a97a1107ace728e76aace06c33bda6ff
Jime1805 <18ejimen@gmail.com>
2026-05-13 18:36:53 +0200

    projecte creat

---
+++ projecte_3/src/test/java/projecte3/projecte_3/Projecte3ApplicationTests.java
@@ -0,0 +1,13 @@
+package projecte3.projecte_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projecte3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
